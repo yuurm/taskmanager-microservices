60386900e6c7aa873630906cddeebbdf5bac1413
yuurm <73988712+yuurm@users.noreply.github.com>
2023-03-02 17:31:07 +0300

    init project repo

---
+++ src/main/java/com/yuurm/taskmanagerutils/TaskmanagerUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.yuurm.taskmanagerutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerUtilsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yuurm/taskmanagerutils/utils/PlannerUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.yuurm.taskmanagerutils.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerUtilsApplication.class, args);
+    }
+
+}
