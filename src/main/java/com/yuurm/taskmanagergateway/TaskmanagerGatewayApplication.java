e986d72a6b9422168052c1f95d48e21c0abffb4d
yuurm <73988712+yuurm@users.noreply.github.com>
2023-03-02 17:31:06 +0300

    init project repo

---
+++ src/main/java/com/yuurm/taskmanagergateway/TaskmanagerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yuurm.taskmanagergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerGatewayApplication.class, args);
+    }
+
+}
